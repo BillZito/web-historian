eb7272708388cb063e6e999ffaf251d8aec5cc22
billzito <billrzito@gmail.com>
2016-10-05 20:23:45 -0700

    Cron to download in background, redirects, all tests pass

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,21 @@
-// Use the code in `archive-helpers.js` to actually download the urls
-// that are waiting.
+#!/usr/local/bin/node
+
+var fs = require('fs');
+var archive = require('/Users/student/Desktop/2016-09-web-historian/helpers/archive-helpers.js');
+
+archive.readListOfUrls( (dataArr) => {
+  archive.downloadUrls(dataArr);
+});
+
+
+var basePath = '/Users/student/Desktop/2016-09-web-historian/workers/log.text';
+if (!fs.existsSync(basePath)) {
+    // We use fs.openSync to create the file
+  var file = fs.openSync(basePath, 'w');
+  fs.writeFile(basePath, 'created the first one');
+  fs.closeSync(file);
+} else {
+  fs.appendFile(basePath, 'we ran one cron');
+}
+
+
